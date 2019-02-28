3be15bfdb5f8a0f5b1f5e3cc058828d661dc9bca
billy-yjh <40520384+billy-yjh@users.noreply.github.com>
2019-02-28 12:31:08 +0800

    Revert "Revert "Revert "1"""
    This reverts commit 925c5e42586fdec3df9d68f2e5e41c7c58430475.

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -7,17 +8,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/ebook'
+      name: 'home',
+      component: Home
     },
     {
-      path: '/ebook',
-      component: () => import('./views/ebook/index.vue'),
-      children: [
-        {
-          path: ':fileName',
-          component: () => import('./components/ebook/EbookReader.vue')
-        }
-      ]
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
