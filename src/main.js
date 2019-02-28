5889c32f33cd7ff20cb7adfa80daddb346c59cbd
billy-yjh <40520384+billy-yjh@users.noreply.github.com>
2019-02-28 10:35:08 +0800

    Revert "1"
    This reverts commit 5b5771cb9b8c4904f65f6d9cf96201922d361d4b.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import i18n from './lang'
-import store from './store/store'
-/*
- *引入字体图标
- */
-import './assets/styles/icon.css'
-/**
- * 引入自定义字体
- */
-// import './assets/fonts/daysOne.css'
-
-import './assets/styles/global.scss'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  i18n,
   store,
   render: h => h(App)
 }).$mount('#app')
